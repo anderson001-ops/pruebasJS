fec4c2fa66adf9c0b50e2b06d2d4d822a7588fe5
anderson <joelcapera2023@gmail.com>
2025-06-17 13:27:28 -0500

    agrego

--- fibonnaci/pruebalet.js
+++ fibonnaci/pruebalet.js
@@ -1,26 +1,33 @@
 /* variable global */
 const fibonacci = {
     inicio: 0, /*valor con el cual empieza */
-    final: 6 /*valor con el cual finaliza */
+    final: 20 /*valor con el cual finaliza */
 };
 
 /* funcion flecha para fibonacci */
 const spawnfibonacci = () => {
-    let serie = [];
-    let a = fibonacci.inicio === 0 ? 0 : 1;
-    let b = 1;
+    const serie =new Set ();
+
+    /*var serie = [];*/
+    var a = fibonacci.inicio === 0 ? 0 : 1;
+    var b = 1;
 
     if (fibonacci.inicio === 0) {
-    serie.push(0);
+    serie.add(0);
+    /*serie.push (0)*/
     }
     while (b <= fibonacci.final) {
     if (b >= fibonacci.inicio) {
-    serie.push(b);
+    serie.add(b);
+    /*serioe.push(b);*/
     }
-    const next = a + b;
+    var next = a + b;
     a = b;
     b = next;
     }
     return serie;
 };
-console.log("Fibonacci en serie ", spawnfibonacci());
+const sfibonacci = spawnfibonacci ();
+
+console.log("Serie Fibonacci", sfibonacci);
+console.log("Como array:", Array.from(sfibonacci));
