cbae77b3966658589a3cb00692d9ccd80da2ca30
Mateo <2030suarezmolano@gmail.com>
2025-06-18 13:29:52 -0500

    arreglo flechas

--- fibonnaci/pruebalet.js
+++ fibonnaci/pruebalet.js
@@ -1,40 +1,65 @@
 const fibonacci = {
-    inicio: 0, /*valor con el cual empieza */
-    final: 20 /*valor con el cual finaliza */
+    inicio: 5, /*valor con el cual empieza */
+    final: 30 /*valor con el cual finaliza */
 };
-
-/* funcion flecha para fibonacci */
-const spawnfibonacci = () => {
-    /*funcion con array */
-
-    /*funcion con set */
-    /* const serie =new Set (); */
-
-    const serie =new Map ();
-    /*var serie = [];*/
+const spawnfibonaccia = () => {
+    var serie = [];
     var a = fibonacci.inicio === 0 ? 0 : 1;
     var b = 1;
-    var indice = 0 ; 
-
     if (fibonacci.inicio === 0) {
-    /* serie.add(0);*/
-    /*serie.push (0)*/
-    serie.set(indice++,0);
+    serie.push(0);
     };
     while (b <= fibonacci.final) {
     if (b >= fibonacci.inicio) {
-    serie .set(indice++,b);
-    /*serie.add(b);*/
-    /*serioe.push(b);*/
+    serie.push(b);
     }
+    const next = a + b;
+    a = b;
+    b = next;
+    }
+    return serie ;
+};
+const resultadoarray = spawnfibonaccia ();
+console.log("Serie Fibonacci", resultadoarray);
+
+const spawnfibonaccib= () => {
+    const serie =new Set ();
+    var a = fibonacci.inicio === 0 ? 0 : 1;
+    var b = 1;
+    if (fibonacci.inicio === 0) {
+    serie.add (0)
+    }
+    while (b <= fibonacci.final) {
+    if (b >= fibonacci.inicio) {
+    serie.add(b);
+    };
     var next = a + b;
     a = b;
     b = next;
     }
-    return serie;
+    return serie ;
 };
-const sfibonacci = spawnfibonacci ();
+const resultadoset=spawnfibonaccib();
+console.log("set",[resultadoset]);
 
-console.log("Serie Fibonacci", sfibonacci);
-console.log("Como array:", Array.from(sfibonacci));
-console.log("Map:", sfibonacci.entries());
+const spawnfibonaccic= () => {
+    const serie =new Map ();
+    var a = fibonacci.inicio === 0 ? 0 : 1;
+    var b = 1;
+    var indice = 0 ;
+    if (fibonacci.inicio === 0) {
+    serie.set(indice++,0);
+    };
+    while (b <= fibonacci.final) {
+    if (b >= fibonacci.inicio) {
+    serie.set(indice++,b);
+    };
+    var next = a + b;
+    a = b;
+    b = next;
+    }
+    return serie ;
+};
+const resultadomap=spawnfibonaccic();
+console.log("map",[resultadomap]);
+
